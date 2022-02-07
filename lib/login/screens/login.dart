79bde5364f6ad8db3968ea37d944457e70bf63dd
Kimberlly Florez <kimberllyflorez@pop-os.localdomain>
2022-02-07 00:06:21 -0500

    New login screen

--- lib/login/screens/login.dart
+++ lib/login/screens/login.dart
@@ -1,4 +1,4 @@
-import 'package:example_whit_github/home/screens/home.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatelessWidget {
@@ -7,36 +7,105 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login'),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          const Center(
-            child: TextField(
-              textAlign: TextAlign.center,
-              decoration: InputDecoration(
-                border: InputBorder.none,
-                hintText: 'Please introduce your first name',
+      body: Container(
+        color: Colors.greenAccent,
+        width: double.infinity,
+        height: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(height: 60),
+            Icon(
+              Icons.star_border,
+              color: Colors.white,
+              size: 50.0,
+            ),
+            Text(
+              'Login',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 60),
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.all(30),
+                width: double.infinity,
+                decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(50),
+                        topLeft: Radius.circular(50))),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        Text(
+                          'Log in ',
+                          style: TextStyle(color: Colors.green, fontSize: 20),
+                        ),
+                        Text(
+                          'your accont.',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                      ],
+                    ),
+                    const Text('Email Adress '),
+                    const TextField(
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(
+                          Icons.email,
+                          color: Colors.green,
+                        ),
+                        border: OutlineInputBorder(),
+                        hintText: 'email',
+                      ),
+                    ),
+                    const Text('Password '),
+                    const TextField(
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.password_rounded,
+                          color: Colors.green,),
+                        border: OutlineInputBorder(),
+                        hintText: 'password',
+                      ),
+                    ),
+
+                    const Text('Forgot PassWord'),
+                    ElevatedButton(
+                      style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStateProperty.all(Colors.green),
+                          shape:
+                              MaterialStateProperty.all<RoundedRectangleBorder>(
+                                  RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(10.0),
+                                      side: BorderSide(color: Colors.green)))),
+                      onPressed: () {
+                        Navigator.pushNamed(context, 'home');
+                      },
+
+                      child: Text('Login'),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        Text('Do not have an account?'),
+                        Text(
+                          'Sing in',
+                          style: TextStyle(color: Colors.green),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-          const Center(
-            child: TextField(
-              textAlign: TextAlign.center,
-              decoration: InputDecoration(
-                  border: InputBorder.none,
-                  hintText: 'Please introduce your second name'),
-            ),
-          ),
-          ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, 'home');
-              },
-              child: const Text('ingresar'))
-        ],
+          ],
+        ),
       ),
     );
   }
