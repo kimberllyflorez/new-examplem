a86c3b3f758f341dc220449ad15abdd43c186bf2
kimberllyflorez <kimberlly.florez>
2022-02-09 16:46:33 -0500

    include navegator bar

--- lib/home/screens/home.dart
+++ lib/home/screens/home.dart
@@ -1,22 +1,66 @@
+import 'package:example_whit_github/login/screens/login.dart';
+import 'package:example_whit_github/profile/screens/profile.dart';
+import 'package:example_whit_github/theme/custom_colors.dart';
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
+
+class Home extends StatefulWidget {
+
   const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _selectIndex = 0;
+
+  static List<Widget> _children = <Widget>[
+    //Home(),
+    Text('sdfsdgdf'),
+    Text('sdfds'),
+
+    Profile(),
+
+  ];
+
+
+  void onTapped(int index) {
+    setState(() {
+      _selectIndex = index;
+    });
+    //pageController.jumpToPage(_children);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-            appBar: AppBar(
+      appBar: AppBar(
         title: const Text('welcom to home'),
       ),
-      body: Center(
-        child: ElevatedButton(
-            onPressed: ()  {
-              Navigator.pushNamed(context, 'profile');
-            },
-            child: const Text('click for edit your profile',
-              textAlign: TextAlign.center,)
-        )
+      body: Container(
+        child: _children[_selectIndex],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+          items:
+          <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.paste_rounded),
+              label: 'login',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.emoji_people),
+              label: 'perfil',
+            )
+          ],
+          currentIndex: _selectIndex,
+          selectedItemColor: CustomColors.primaryColor,
+          onTap: onTapped,
+
       ),
     );
   }
